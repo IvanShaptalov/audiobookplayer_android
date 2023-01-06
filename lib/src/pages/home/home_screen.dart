8407d6e369fe952763469ff564920970701fc1bb
devIS <ivan.shaptalov1@gmail.com>
2023-01-06 10:20:02 +0200

    feat(home_screen.dart -> Column): created composition

--- lib/src/pages/home/home_screen.dart
+++ lib/src/pages/home/home_screen.dart
@@ -8,7 +8,11 @@ import '../sample_item_details_view.dart';
 class SampleItemListView extends StatelessWidget {
   const SampleItemListView({
     super.key,
-    this.items = const [SampleItem(1, 'Audiobooks1'), SampleItem(2, 'Audiobook2'), SampleItem(3, 'Audiobook3')],
+    this.items = const [
+      SampleItem(1, 'Audiobooks1'),
+      SampleItem(2, 'Audiobook2'),
+      SampleItem(3, 'Audiobook3')
+    ],
   });
 
   static const routeName = '/';
@@ -67,41 +71,47 @@ class SampleItemListView extends StatelessWidget {
         // building all Widgets up front, the ListView.builder constructor lazily
         // builds Widgets as they’re scrolled into view.
 
-        body: Container(
-          height: 130,
-          child: ListView.builder(
-            // Providing a restorationId allows the ListView to restore the
-            // scroll position when a user leaves and returns to the app after it
-            // has been killed while running in the background.
-            restorationId: 'sampleItemListView',
-            scrollDirection: Axis.horizontal,
-            itemCount: items.length,
-            itemBuilder: (BuildContext context, int index) {
-              final item = items[index];
+        body: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / items.length,
+              child: ListView.builder(
+                // Providing a restorationId allows the ListView to restore the
+                // scroll position when a user leaves and returns to the app after it
+                // has been killed while running in the background.
+                restorationId: 'sampleItemListView',
+                scrollDirection: Axis.horizontal,
+                itemCount: items.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final item = items[index];
 
-              return Container(
-                  margin: EdgeInsets.symmetric(vertical: 20.0),
-                  width: MediaQuery.of(context).size.width / items.length,
-                  height: MediaQuery.of(context).size.height / items.length*3,
-                  color: Color.fromARGB(179, 48, 194, 109),
-                  child: ListTile(
-                      title: Text('${item.title}'),
-                      leading: const CircleAvatar(
-                        // Display the Flutter Logo image asset.
-                        foregroundImage:
-                            AssetImage('assets/images/flutter_logo.png'),
-                      ),
-                      onTap: () {
-                        // Navigate to the details page. If the user leaves and returns to
-                        // the app after it has been killed while running in the
-                        // background, the navigation stack is restored.
-                        Navigator.restorablePushNamed(
-                          context,
-                          SampleItemDetailsView.routeName,
-                        );
-                      }));
-            },
-          ),
+                  return Container(
+                      margin: EdgeInsets.symmetric(vertical: 20.0),
+                      width: MediaQuery.of(context).size.width / items.length,
+                      color: Color.fromARGB(179, 48, 194, 109),
+                      child: ListTile(
+                          title: Text('${item.title}'),
+                          leading: const CircleAvatar(
+                            // Display the Flutter Logo image asset.
+                            foregroundImage:
+                                AssetImage('assets/images/flutter_logo.png'),
+                          ),
+                          onTap: () {
+                            // Navigate to the details page. If the user leaves and returns to
+                            // the app after it has been killed while running in the
+                            // background, the navigation stack is restored.
+                            Navigator.restorablePushNamed(
+                              context,
+                              SampleItemDetailsView.routeName,
+                            );
+                          }));
+                },
+              ),
+            )
+          ,
+          Text('new row'),
+          Text('new row')],
+          
         ));
   }
 }
