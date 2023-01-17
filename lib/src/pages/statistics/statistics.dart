db9a28befc1d8f75b04357f128630a346ce0a195
devIS <ivan.shaptalov1@gmail.com>
2023-01-17 21:14:43 +0200

    feat(statistics.dart): created statistics routing;

--- lib/src/pages/statistics/statistics.dart
+++ lib/src/pages/statistics/statistics.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+
 class Statistics extends StatelessWidget {
   static const String routeName = "/statistics";
   @override
   Widget build(BuildContext context) {
-    
-    return Scaffold(appBar: AppBar(title: Text('Full stats'), ),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Full stats'),
+      ),
+    );
   }
 }
