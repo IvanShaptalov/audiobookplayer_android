94d35e05c8ca0a8c83e23c326c35988681a07eb9
devIS <ivan.shaptalov1@gmail.com>
2023-01-20 20:57:17 +0200

    feat(player.dart): use just_audio 0.9.31 player, to play sound from local files;

--- lib/src/pages/home/view/player.dart
+++ lib/src/pages/home/view/player.dart
@@ -2,29 +2,18 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:audiobook_player/src/config/config.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class Player extends StatelessWidget {
   AudioPlayer player = AudioPlayer();
 
-  Player({super.key});
+  void playAudio() async {
+    String musicPath = CurrentPlayingMusicConfig.getMusic;
+    final duration = await player.setFilePath(musicPath);
+    player.play();
 
-  Future<int> playAudio() async {
-    String audioasset = CurrentPlayingMusicConfig.getMusic;
-    ByteData bytes = await rootBundle.load(audioasset); //load audio from assets
-    Uint8List audiobytes =
-        bytes.buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes);
-    int result = await player.playBytes(audiobytes);
-    if (result == 1) {
-      //play success
-      print("audio is playing.");
-    } else {
-      print("Error while playing audio.");
-    }
-    print('all ok playing');
-    return result;
   }
 
   // int result = player.playBytes(audiobytes);
@@ -36,7 +25,7 @@ class Player extends StatelessWidget {
         icon: const Icon(Icons.play_arrow),
         onPressed: () {
           playAudio();
-          // print("good, audio played");
+          print("good, audio played");
         },
       ),
       
